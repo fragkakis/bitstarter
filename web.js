1b2b4fd450312eed71920b90a22b9dbe6516af60
fragkakis <markos.fragkakis@gmail.com>
2013-07-10 21:13:57 +0000

    Now reading response from file

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var responseStr = fs.readFileSync('index.html').toString();
+
+  response.send(responseStr);
 });
 
 var port = process.env.PORT || 5000;
